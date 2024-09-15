16920fca0e60833e41800f90ab1ddf20bf6c63d8
Nonym0usse <cyril.vella@yahoo.com>
2024-09-15 12:50:19 +0200

    upgrade fireauth + debug

--- database/auth.js
+++ database/auth.js
@@ -8,17 +8,20 @@ const verifyTokenMiddleware = async (req, res, next) => {
     }
 
     const token = authHeader.split(' ')[1];
-    console.log(token)
     try {
         req.decodedToken = await admin.auth().verifyIdToken(token);
         next();
     } catch (error) {
-        if (error.code === 'auth/id-token-revoked') {
-            console.log('Le token à expiré')
+        if (error.code === 'auth/id-token-expired') {
+            console.log('Firebase ID token has expired:', error);
+            return res.status(401).json({ message: 'Firebase ID token has expired' });
+        } else if (error.code === 'auth/id-token-revoked') {
+            console.log('Firebase ID token has been revoked:', error);
+            return res.status(401).json({ message: 'Firebase ID token has been revoked' });
         } else {
-            console.log(error)
+            console.error('Error verifying Firebase ID token:', error);
+            return res.status(500).json({ message: 'Failed to verify token' });
         }
-        return res.status(401).json({ message: 'Invalid token' });
     }
 };
 
