d77aa1a3b2d566b7ff9cc310faede74acc536c44
Nonym0usse <cyril.vella@yahoo.com>
2025-01-19 19:58:38 +0100

    update email

--- middleware/admin/Contact.js
+++ middleware/admin/Contact.js
@@ -2,37 +2,43 @@ const firebase = require('../../database/firebase');
 const nodemailer = require('nodemailer')
 const fs = require('fs');
 const ejs = require('ejs');
-const path = require("path");
-const {get} = require("axios");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    host: 'mail.colocservice.fr',
+    service: 'mail.creatoke.net',
+    host: 'mail.creatoke.net',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.user,
+        pass: process.env.password,
+    },
+});
+
+function mailOptions(sender, subject, emailHtml) {
+    return {
+        from: process.env.user,
+        to: sender,
+        subject: subject,
+        html: emailHtml
+    }
+};
+
 class Contact {
-    music =  [];
+    music = [];
     async sendEmail(data) {
         new Promise(async (resolve, reject) => {
             if (!data.email || !data.name || !data.message || !data.tel) {
                 throw Error('INVALID_PARAMS');
             }
-            const transporter = nodemailer.createTransport({
-                host: 'mail.colocservice.fr',
-                port: 465, // Use the appropriate port (587 for TLS or 465 for SSL)
-                secure: true, // true for 465, false for other ports
-                auth: {
-                    user: 'contact@colocservice.fr', // Replace with your OVH email address
-                    pass: 'sT100573!', // Replace with your OVH email password or application-specific password
-                },
-
-            });
             const image = await fs.promises.readFile('templates/logo.png');
             data.image = `data:image/jpeg;base64,${image.toString('base64')}`;
             const emailHtml = await this.renderEmailTemplate('templates/contact.ejs', data);
-            const mailOptions = {
-                from: 'colocservice@hotmail.com', // Sender's email address
-                to: data.email, // Recipient's email address
-                subject: "CREATOKE : ✉️ Nouveau message" , // Email subject
-                html: emailHtml
-            };
 
-            // Send the email
-            await transporter.sendMail(mailOptions, (error, info) => {
+            await transporter.sendMail(mailOptions(data.email, "CREATOKE : ✉️ Nouveau message", emailHtml), (error, info) => {
                 if (error) {
                     console.log(error)
                     reject(error)
@@ -45,27 +51,10 @@ class Contact {
 
     async sendEmailPayment(data) {
         new Promise(async (resolve, reject) => {
-            const transporter = nodemailer.createTransport({
-                host: 'mail.colocservice.fr',
-                port: 465, // Use the appropriate port (587 for TLS or 465 for SSL)
-                secure: true, // true for 465, false for other ports
-                auth: {
-                    user: 'contact@colocservice.fr', // Replace with your OVH email address
-                    pass: 'sT100573!', // Replace with your OVH email password or application-specific password
-                },
-
-            });
             const imageBuffer = await fs.promises.readFile('templates/logo.png');
             data.logo = imageBuffer.toString('base64');
-
             const emailHtml = await this.renderEmailTemplate('templates/purchase-confirm.ejs', data);
-            const mailOptions = {
-                from: 'contact@colocservice.fr',
-                to: data.email_client,
-                subject: "Créatoké : Merci pour votre achat." , // Email subject
-                html: emailHtml,
-            };
-            await transporter.sendMail(mailOptions, (error, info) => {
+            await transporter.sendMail(mailOptions(data.email_client, "Créatoké : Merci pour votre achat.", emailHtml), (error, info) => {
                 if (error) {
                     console.log(error)
                     reject(error)
