3010d2fe2af76b5aa40aef13d623ecca6c3ba5c4
Nonym0usse <cyril.vella@yahoo.com>
2025-03-09 18:04:16 +0100

    removing download route

--- routes/index.js
+++ routes/index.js
@@ -12,44 +12,5 @@ router.get('/', function (req, res, next) {
     .catch(e => res.status(500).json(e))
 });
 
-router.post('/download', async (req, res) => {
-  try {
-    const songUrl = req.body.songUrl;
-    const songName = req.body.songName || 'song';
-
-    if (!songUrl) {
-      return res.status(400).json({ error: 'Missing songUrl parameter' });
-    }
-
-    const response = await fetch(songUrl);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch file: ${response.statusText}`);
-    }
-
-    const contentType = response.headers.get('content-type') || response.headers.get('Content-Type') || 'audio/mpeg';
-    
-    const data = await response.arrayBuffer();
-    const buffer = Buffer.from(data);
-    console.log(contentType)
-    let fileExtension = '.mp3'; // Default to MP3
-    if (contentType.includes('wav')) {
-      fileExtension = '.wav';
-    } else if (contentType.includes('ogg')) {
-      fileExtension = '.ogg';
-    } else if (contentType.includes('aac')) {
-      fileExtension = '.aac';
-    }
-
-    const safeFilename = encodeURIComponent(songName.replace(/[^a-zA-Z0-9-_]/g, '_')) + fileExtension;
-
-    res.setHeader('Content-Type', contentType);
-    res.setHeader('Content-Disposition', `attachment; filename="${safeFilename}"`);
-
-    res.send(buffer);
-  } catch (error) {
-    console.error('Download error:', error);
-    res.status(500).json({ error: error });
-  }
-});
 
 module.exports = router;
