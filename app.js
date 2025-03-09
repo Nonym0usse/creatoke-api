ea64a21c13d1b1288208cc5baa0fa53afbf38933
Nonym0usse <cyril.vella@yahoo.com>
2025-03-09 17:19:06 +0100

    cors 2

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ var renewTokenRouter = require('./routes/admin/renew-token');
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Adjust for security
@@ -28,6 +23,13 @@ app.use((req, res, next) => {
   res.header('Access-Control-Expose-Headers', 'Content-Type'); // Ensure headers are visible
   next();
 });
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
